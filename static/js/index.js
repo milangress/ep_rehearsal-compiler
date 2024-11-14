6c1f42bf379f7b134de6753eed93c837e3d25b16
Milan G <dev@milangress.de>
2024-11-14 10:41:27 +0100

    f

--- static/js/index.js
+++ static/js/index.js
@@ -5,4 +5,14 @@ exports.postAceInit = function(hook, context){
     console.log("postAceInit && ep__rehearsal-compiler init");
     console.log(context);
     console.log(hook);
-  };
+    return;
+};
+
+
+exports.postToolbarInit = (hookName, context) => {
+  const editbar = context.toolbar; // toolbar is actually editbar - http://etherpad.org/doc/v1.5.7/#index_editbar
+  editbar.registerCommand('run', () => {
+    console.log("run");
+  });
+  return true;
+};
