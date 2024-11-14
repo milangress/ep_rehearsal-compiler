280ae952e9053bbfb54d9b52cf05e695a7eccfc4
Milan G <dev@milangress.de>
2024-11-14 10:21:15 +0100

    f

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,7 @@
-var postAceInit = function(hook, context){
+'use strict';
+
+
+exports.postAceInit = function(hook, context){
     console.log("postAceInit && ep__rehearsal-compiler init");
     console.log(context);
     console.log(hook);
